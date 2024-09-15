c7be878fb4b3dccbbb757b7bd1e5c888a4a91239
Aleximbrea <aleximbrea4@gmail.com>
2024-09-15 09:16:54 +0200

    Added functions to get daily and 5min bars

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Stock filters
+# Stock screeners
 Filter stocks that are part of the s&p500 index
+
+## Get started
+
+## How it works
+
+### Dependencies
+- Using **yfinance** library to get stock data
+- Importing s&p500 stocks from **wikipedia**
--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 import yfinance as yf
 import pandas as pd
+from datetime import datetime, timedelta
 
 # List of s&p500 stocks
 try:
@@ -7,6 +8,34 @@ try:
 except:
     raise Exception("Error on getting stock tickers")
 
+def getDailyBars(ticker, start=None, end=None):
+
+    # If no start date is given we get data starting from 30 days ago
+    if not start:
+        start = datetime.now()  - timedelta(days=30)
+        start = start.strftime('%Y-%m-%d')
+
+    data = yf.download(ticker, start=start, end=end, interval='1d')
+    return data
+
+def get5MinBars(ticker, start=None, end=None):
+
+    if not start and not end:
+        count = 1
+        # We go back in the days until some data is given
+        while True:
+            # If no start or end date is given we get data from the day before
+            start = datetime.now()  - timedelta(days=count+1)
+            start = start.strftime('%Y-%m-%d')
+            end = datetime.now()  - timedelta(days=count)
+            end = end.strftime('%Y-%m-%d')
+            data = yf.download(ticker, start=start, end=end, interval='5m')
+            if not data.empty:
+                break
+            else:
+                count =+ 1
+    return data
+
 
 if __name__ == "__main__":
-    print(stocks)
+    print(get5MinBars('SPY'))
